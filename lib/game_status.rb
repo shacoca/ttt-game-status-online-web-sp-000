6ce49cd7ad1e9dc62dd181708dae134c5ab881b7
shawn campbell <shacoca@yahoo.com>
2020-08-05 18:42:48 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,12 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
 
-# Define your WIN_COMBINATIONS constant
+  [0,3,6], #left column
+  [1,4,7], #middle column
+  [2,5,8], #right column
+
+  [0,4,8], #NW - SE
+  [2,4,6] #NE - SW
+]
