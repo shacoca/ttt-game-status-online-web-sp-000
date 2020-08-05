11e73185e55073ebfbe1f430a4882597e97c0556
shawn campbell <shacoca@yahoo.com>
2020-08-05 18:56:18 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,3 +10,14 @@ WIN_COMBINATIONS = [
   [0,4,8], #NW - SE
   [2,4,6] #NE - SW
 ]
+
+def won?(board)
+# Iterate over the possible win combinations defined in `WIN_COMBINATIONS` and
+# check if the board has the same player token in each index of a winning
+# combination.
+# enumerate WIN_COMBINATIONS; isolate each win_combo
+  WIN_COMBINATIONS.each do |win_comb0|
+# compare win_combo to corresponding board positions
+# if they match, that's a win, if not, keep enumerating
+
+end
